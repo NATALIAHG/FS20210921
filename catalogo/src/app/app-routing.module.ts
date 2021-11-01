faf676feb68275ddf247edec0daf19b58f0f2a75
NATALIAHG <herasnatalia@hotmail.com>
2021-11-01 22:00:31 +0100

    cambios

--- catalogo/src/app/app-routing.module.ts
+++ catalogo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActoresComponent } from './actores/actores.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'actores', children: [
+    { path: '',  component: ActoresComponent },
+ //   { path: 'add',  component: ContactosAddComponent },
+  //  { path: ':id/edit',  component: ContactosEditComponent },
+  //  { path: ':id',  component: ContactosViewComponent }, // aqui el seo se podria mas datos de ruta s que quiere
+ //   { path: ':id/:kk',  component: ContactosViewComponent }, // aqui el seo se podria mas datos de ruta s que quiere
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ catalogo/src/app/main/main.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { NotificationComponent } from './notification/notification.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { SecurityModule } from '../security';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+ //   NotificationComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+  ],
+  exports: [
+    HomeComponent, NotificationComponent, PageNotFoundComponent, HeaderComponent,
+  ],
+  imports: [
+    CommonModule, RouterModule.forChild([]),
+    SecurityModule,
+
+  ]
+})
+export class MainModule { /* PARA QUE SOLO SE CARGUE UNA VEZ EL MAIN EN LA PAGINA, poner parentesis al final*/
+  constructor( @Optional() @SkipSelf() parentModule: MainModule) {
+    if (parentModule) {
+      const msg = `MainModule has already been loaded.
+        Import MainModule once, only, in the root AppModule.`;
+      throw new Error(msg);
+    }
+  }
+}
